6b9d9ae119587c302016ffa043fd35cc113d2436
Luana Garcia Mathias <124801836+Luana1c@users.noreply.github.com>
2025-02-23 18:17:03 -0300

    Feito exercício07.php

---
+++ Aula 02/exercício07.php
@@ -0,0 +1,10 @@
+echo "Exercicio 8" . "<br>";
+
+// Recebe um número do usuário
+$numero = 3;
+
+// Exibe a tabuada do número de 1 a 10
+echo "Tabuada de $numero:\n";
+for ($i = 1; $i <= 10; $i++) {
+    echo "$numero x $i = " . ($numero * $i) . "\n" . "<br>";
+}
