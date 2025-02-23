192bb99466c42e54ee64d35569adc1a9680a78e8
Luana Garcia Mathias <124801836+Luana1c@users.noreply.github.com>
2025-02-23 18:15:18 -0300

    Update exercício04.php
    feito

---
+++ Aula 02/exercício04.php
@@ -0,0 +1,16 @@
+echo "Exercicio 4" . "<br>";
+
+$opcao = 2;
+switch ($opcao) {
+    case 1:
+        echo "Home escolhida";
+        break;
+    case 2:
+        echo "Menu escolhida";
+        break;
+    case 3:
+        echo "Contato escolhido";
+        break;
+    default:
+        echo "Opção inválida";
+} 
